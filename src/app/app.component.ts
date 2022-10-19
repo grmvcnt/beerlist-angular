02fdde7654c2a4d54610f5ac61cf7f790073aade
shuang <shuang.zhuang@aramisauto.com>
2022-10-19 21:45:35 +0200

    prepare data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Product } from "../model/product";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-app-s2';
+  public products: Product[] = [];
+
+  constructor(private httpClient: HttpClient) {
+    this.fetchProducts();
+  }
+
+  public fetchProducts() {
+    this.httpClient.get<Product[]>('https://fakestoreapi.com/products')
+      .subscribe((res: Product[]) => {
+        this.products = res.slice(0, 6);
+      });
+  }
 }
